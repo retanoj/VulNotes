1d9fd996cbbde7d359ebc75898eb576117d33c4f
retanoj <s919student@gmail.com>
2016-12-20 22:09:23 +0800

    fix getDate() to getUTCDate()

--- frontend/src/filters.js
+++ frontend/src/filters.js
@@ -38,7 +38,7 @@ exports.formatDate = time => {
   let tmpDate = new Date(time)
   let year = tmpDate.getFullYear()
   let mathon = tmpDate.getMonth() + 1
-  let day = tmpDate.getDate()
+  let day = tmpDate.getUTCDate()
   let hours = tmpDate.getHours()
   let minutes = tmpDate.getMinutes()
   // return year + '.' + mathon + '.' + day + ' ' + hours + ':' + minutes
